b4f1cf28fdc72fd14110cd1406791de86fddd4f4
Luis Calle <lcalle101qc@gmail.com>
2018-05-18 11:43:40 -0400

    tableview working

---
+++ Crayons/Controllers/CrayonsViewController.m
@@ -0,0 +1,56 @@
+//
+//  CrayonsViewController.m
+//  Crayons
+//
+//  Created by Luis Calle on 5/18/18.
+//  Copyright © 2018 Alex Paul. All rights reserved.
+//
+
+#import "CrayonsViewController.h"
+#import "CrayonsView.h"
+#import "Crayon.h"
+
+#define CELLID "crayon cell"
+
+@interface CrayonsViewController ()
+
+@property (nonatomic) CrayonsView *crayonsView;
+
+@end
+
+@implementation CrayonsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    if(!_crayonsView) {
+        _crayonsView = [[CrayonsView alloc] initWithFrame:UIScreen.mainScreen.bounds];
+        _crayonsView.crayonsTableView.delegate = self;
+        _crayonsView.crayonsTableView.dataSource = self;
+    }
+    [self.view addSubview: _crayonsView];
+    self.title = @"Crayons";
+}
+
+
+#pragma mark - UITableView Data Source Methods
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    UITableViewCell *crayonCell = [tableView dequeueReusableCellWithIdentifier:@CELLID forIndexPath:indexPath];
+    
+    if (crayonCell) {
+        crayonCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@CELLID];
+        Crayon *crayon = [Crayon allTheCrayons][indexPath.row];
+        UIColor *bgColor = [UIColor colorWithRed:[crayon red]/255 green:[crayon green]/255 blue:[crayon blue]/255 alpha:1.0];
+        crayonCell.backgroundColor = bgColor;
+        [crayonCell.textLabel setText:[crayon name]];
+        [crayonCell.detailTextLabel setText:[crayon hex]];
+    }
+    return crayonCell;
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [Crayon allTheCrayons].count;
+}
+
+@end
---
+++ Crayons/Views/CrayonsView.h
@@ -0,0 +1,15 @@
+//
+//  CrayonsView.h
+//  Crayons
+//
+//  Created by Luis Calle on 5/18/18.
+//  Copyright © 2018 Alex Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CrayonsView : UIView
+
+@property (nonatomic) UITableView *crayonsTableView;
+
+@end
